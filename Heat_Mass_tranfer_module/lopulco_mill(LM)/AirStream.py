368be2cae026bc35477893572f13559d65f4c473
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-04-22 15:54:42 +0200

    Addition of more methods
    Added volumetric controls

--- Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
+++ Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
@@ -1,6 +1,18 @@
-#funtion to find average of all the flow velocity 
+#funtion to find average of all the flow velocity (1 to 10)
+from cmath import pi
 from statistics import mean
 def Average_Flow_Velocity(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10):
     return mean([v1,v2,v3,v4,v5,v6,v7,v8,v9,v10])
 
-  
+
+#funtion to find Volumetric Flow in pipes according to the  selection of pipes
+#argument 1 = duct selection value
+#argument2 = Duct Dimension 1
+#argument3 = Duct Dimension 2
+#argument 4 = Average Flow Velocity
+
+def Volumetric_Flow(duct_selection_value,Duct_Dimension_1,Duct_Dimension_2,mean_Flow_Velocity):
+    if duct_selection_value ==1: # the selected duct is circular
+        return  mean_Flow_Velocity*pi*pow(Duct_Dimension_1,2)/4
+    else:
+        return Duct_Dimension_1*Duct_Dimension_2*mean_Flow_Velocity
