3dd94df5f26ed091c167d03f0299269564a4347e
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-04-22 17:12:27 +0200

    Retrun  value is been rounded

--- Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
+++ Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
@@ -6,13 +6,17 @@ def Average_Flow_Velocity(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10):
 
 
 #funtion to find Volumetric Flow in pipes according to the  selection of pipes
-#argument 1 = duct selection value
+#argument 1 = duct selection value (1==Circular and 2==square)
 #argument2 = Duct Dimension 1
 #argument3 = Duct Dimension 2
 #argument 4 = Average Flow Velocity
 
 def Volumetric_Flow(duct_selection_value,Duct_Dimension_1,Duct_Dimension_2,mean_Flow_Velocity):
     if duct_selection_value ==1: # the selected duct is circular
-        return  mean_Flow_Velocity*pi*pow(Duct_Dimension_1,2)/4
+        return  round(mean_Flow_Velocity*pi*pow(Duct_Dimension_1,2)/4,3)
     else:
-        return Duct_Dimension_1*Duct_Dimension_2*mean_Flow_Velocity
+        return  round(Duct_Dimension_1*Duct_Dimension_2*mean_Flow_Velocity,2)
+
+from ambient_condition import W
+def humidity(Dry_Bulb_Temperature,Wet_Bulb_Temperature,sea_pressure):
+    return W(Dry_Bulb_Temperature,Wet_Bulb_Temperature,sea_pressure)
