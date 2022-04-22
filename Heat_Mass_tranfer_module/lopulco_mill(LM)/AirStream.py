0302328b5605a633baa8bdfadf3e8cfa9f89e909
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-04-22 14:44:47 +0200

    Air stream Calculations are made here

---
+++ Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
@@ -0,0 +1,6 @@
+#funtion to find average of all the flow velocity 
+from statistics import mean
+def Average_Flow_Velocity(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10):
+    return mean([v1,v2,v3,v4,v5,v6,v7,v8,v9,v10])
+
+  
