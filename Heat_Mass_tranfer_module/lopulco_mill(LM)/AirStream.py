7906c67cf19f31aedd0198accf6655bc9c2924aa
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-04-29 17:28:06 +0200

    update airstream

--- Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
+++ Heat_Mass_tranfer_module/lopulco_mill(LM)/AirStream.py
@@ -30,9 +30,11 @@ def Dry_air_mass_flow(Humidity,total_mass_flow):
 def water_mass_flow(Humidity,total_mass_flow):
     return round(((Humidity*.001)/(1+(Humidity*.001)))*total_mass_flow,3)
     
-#Specific Heat Capacity =F(TAB Status,Dry Bulb Temperature,Dry Mass Flow,Total Mass Flow,Water MAss Flow, Energy Load)
-def specific_heat_capacity(tab_status,dry_bulb_temp,dry_mass_flow,total_mass_flow,water_mass_flow,energy_load):
-    if tab_status=="True":
-       return round( ( ( ((0.0000005*pow(dry_bulb_temp,2)) + (-0.000007*dry_bulb_temp) + 1.0053)*(dry_mass_flow/total_mass_flow) ) + ( ((0.0000008*pow(dry_bulb_temp,2)) + (0.0002*dry_bulb_temp) + 1.8572)*(water_mass_flow/total_mass_flow)  )),3)
-    else:
-       return round(  (energy_load-(2257*water_mass_flow))/(total_mass_flow*dry_bulb_temp),3 )
+#Specific Heat Capacity =F(TAB Status,Dry Bulb Temperature,Dry Mass Flow,Total Mass Flow,Water MAss Flow, Energy Load)      
+def specific_heat_capacity(tabStatus,dryBulbTemp,dryMassFlow,totalMassFlow,waterMassFlow,energyLoad):
+        if tabStatus== True:
+            results = (((0.0000005*(dryBulbTemp**2))+(-0.000007*dryBulbTemp) +1.0053)*(dryMassFlow/totalMassFlow)) +(((0.0000008*(dryBulbTemp**2)) + (0.0002*dryBulbTemp) +1.8572)*(waterMassFlow/totalMassFlow))
+            return round(results,4)
+        elif tabStatus== False:
+            results = ((energyLoad-(2257*waterMassFlow))/(totalMassFlow*dryBulbTemp))
+            return round(results,4)
