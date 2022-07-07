9340fb3b25f427e24f8d111d4b1eff3346e5b832
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-07-07 17:49:56 +0200

    ....

--- GP_Model_HM/Grinder_calciner/Input_material.py
+++ GP_Model_HM/Grinder_calciner/Input_material.py
@@ -4,6 +4,18 @@
 Dry_flow = lambda Gypsum_Wet_gypsum_flow,Gypsum_Moisture : round(Gypsum_Wet_gypsum_flow*(1 - 0.01*Gypsum_Moisture)*1000/3600 ,2)
 #-----------------------------------------
 #Liquid water flow (kg/s)
+#'Gypsum_Moisture' comes from IN4.ORM
 Liquid_water_flow = lambda Gypsum_Wet_gypsum_flow,Gypsum_Moisture : round(Gypsum_Wet_gypsum_flow*(0.01*Gypsum_Moisture)*1000/3600 ,2)
+#-----------------------------------------
+#Energy flow (Kcal/s)
+#temp_ambient= the ambient temperature of site elevation
+#purity_Gypsum = Gypsum Purity percentage
+#dry_flow_input_material = Mill grinder Input material 'Dry flow'
+#liquid_water_flow_input_flow =  Mill grinder Input material 'Liquid Water Flow'
 
-print(Dry_flow(29.1,1.4),Liquid_water_flow(29.1,1.4) )
+def Energy_flow(temp_ambient,purity_Gypsum,dry_flow_input_material ,liquid_water_flow_input_flow):
+    return round(SHSolid(temp_ambient, (0.01*purity_Gypsum)*dry_flow_input_material, 0, 0, (1-0.01*purity_Gypsum)*dry_flow_input_material, liquid_water_flow_input_flow) )
+
+SHSolid = lambda T , MG , MP , MA , MI , MW  : (21.84 * T + 0.076 * (T ** 2 / 2 + 273.15 * T)) / 172.171 * MG + (11.48 * T + 0.061 * (T**2 / 2 + 273.15 * T)) / 145.148 * MP + (14.01 * T + 0.033 * (T**2 / 2 + 273.15 * T)) / 136.138 * MA + (21.84 * T + 0.076 * (T**2 / 2 + 273.15 * T)) / (172.171) * MI + 18.0153 * T / 18.0153 * MW
+
+
--- GP_Model_HM/Mill_outlet.py
+++ GP_Model_HM/Mill_outlet.py
@@ -3,4 +3,4 @@
 # argument 2 = bridge_flow_dry_flow
 # argument 3 = bridge_o2_estimate_percentage
 
-O2_estimate_percentage = lambda air_ingress_Mill,bridge_flow_dry_flow,bridge_o2_estimate_percentage : round(100*(0.01*bridge_o2_estimate_percentage*bridge_flow_dry_flow+0.209*air_ingress_Mill)/(bridge_flow_dry_flow+air_ingress_Mill),1)
+O2_estimate_percentage = lambda air_ingress_mill,bridge_flow_dry_flow,bridge_o2_estimate_percentage : round(100*(0.01*bridge_o2_estimate_percentage*bridge_flow_dry_flow+0.209*air_ingress_mill)/(bridge_flow_dry_flow+air_ingress_Mill),1)
