e8aa9d2efbcedb3667a89b69378affbd90526259
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-07-11 11:21:41 +0200

    Weather data Scarpping

--- GP_Model_HM/Burner flow.py
+++ GP_Model_HM/Burner flow.py
@@ -6,9 +6,8 @@ def MVOL(Temperature,Humidity,Static_Pressure):
         return abs(round(MVOL,2))  # Round the value to 2 decimal points
 
 #Stochiometric_air_factor = Constant = 9.8
-# Gas_flow
+# Gas_flow = Tag
 
-
+dry_flow_combustion_air =  volume_flow_combustion_air * MVOL(X40,X42,Pressure_Pa_To_mmWC(100*$D$6-PATM())) / ( 1 + humidity_ambient / 1000 )
 excess_air= lambda dry_flow_combustion_air,Gas_flow,Stochiometric_air_factor: round(100*(dry_flow_combustion_air/MVOL(0, 0, 0)*3600/(Gas_flow*Stochiometric_air_factor) - 1),1)
-o2_estimate = lambda excess_air,dry_flow_combustion_air,dry_flow_bridge_flow  : round(100*excess_air/(100 + excess_air)*dry_flow_combustion_air*0.209/dry_flow_bridge_flow ,1 )
-print(o2_estimate(55.6,3.28,8.03))
+#o2_estimate = lambda excess_air,dry_flow_combustion_air,dry_flow_bridge_flow  : round(100*excess_air/(100 + excess_air)*dry_flow_combustion_air*0.209/dry_flow_bridge_flow ,1 )
