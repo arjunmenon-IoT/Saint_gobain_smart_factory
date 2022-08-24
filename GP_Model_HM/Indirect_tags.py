868bcf6d234fc5c867eff67361bb61a5c3ac4796
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-08-24 09:58:52 +0200

    Udpates

--- GP_Model_HM/Indirect_tags.py
+++ GP_Model_HM/Indirect_tags.py
@@ -1,8 +1,7 @@
 #volumetric_flow = ((D13-(D13*(D10/100)))*(D11/100))*(D5/D4) #d13 = TAG
 
 # View number 6
-#Stucco_flow_t_h = ((tag-(tag*(gypsum_Moisture/100)))*(gypsum_purity/100))*(145/172)  #tag  = Mill_Feed.Kukla_ActualFlow (Feed rate of rock input )
-
+Stucco_flow_t_h = ((tag-(tag*(gypsum_Moisture/100)))*(gypsum_purity/100))*(145/172)  #tag  = Mill_Feed.Kukla_ActualFlow (Feed rate of rock input )
 
 
 """
