54345a1f707cbf57153de8ab0ad9bedfacbb9e22
Arjun P Menon <78360237+arjunmenon-IoT@users.noreply.github.com>
2022-09-19 10:15:53 +0200

    While Loop "Optimezer" Removed

--- GP_Model_HM/Model_Convergence(Succes).py
+++ GP_Model_HM/Model_Convergence(Succes).py
@@ -266,22 +266,24 @@ while ((energy_Error != 0 or dry_flow_Error != 0 or water_Error != 0) ) :
     energy_Error =  round(100*(energy_inputs-energy_outputs)/energy_inputs)
     dry_flow_Error = round(100*(dry_flow_inputs-dry_flow_outputs)/dry_flow_inputs)
     water_Errors = round(100*(water_iputs-water_outputs)/water_iputs)
+    optimised = False # Link to next Stage of caculation
    # print( [energy_Error, dry_flow_Error, water_Errors] )
     if(energy_Error == 0 and dry_flow_Error==0 and water_Errors==0):
         break
 
 
 
-TAG_HUMIDITY = 400
+TAG_HUMIDITY = 300
 TAG_GAS_FLOW = 600
 optimised = False
 while (optimised == False):
         while(HUMIDITY!=TAG_HUMIDITY):
-                AIR_INGRESS_FILTER= AIR_INGRESS_FILTER+ 1
+                AIR_INGRESS_FILTER= AIR_INGRESS_FILTER+ .00001
+                print(AIR_INGRESS_FILTER)
         while (TAG_GAS_FLOW!=GAS_FLOW):
                 AIR_INGRESS_MILL = AIR_INGRESS_MILL+ 1
                 AIR_INGRESS_FILTER = AIR_INGRESS_FILTER- AIR_INGRESS_MILL
-        optimised==True
+        optimised=True
 
 
-print(round(recirculation_air_density,2),round(recirculated_air_temp,1),round(recirculation_air_dry_flow,2),round (HUMIDITY),round(recirculated_air_energy_flow),round(GAS_FLOW),round(AIR_INGRESS_MILL),round(AIR_INGRESS_FILTER))
+#print(round(recirculation_air_density,2),round(recirculated_air_temp,1),round(recirculation_air_dry_flow,2),round (HUMIDITY),round(recirculated_air_energy_flow),round(GAS_FLOW),round(AIR_INGRESS_MILL),round(AIR_INGRESS_FILTER))
